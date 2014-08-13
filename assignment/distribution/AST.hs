448ff174f003550ac9d6cef2245c26e4386fed7c
Zachary May <zach@sector42.net>
2014-08-13 16:04:44 -0400

    Some final tweaks

--- assignment/distribution/AST.hs
+++ assignment/distribution/AST.hs
@@ -1,7 +1,19 @@
 module AST where
 
+-- | A LANG program consists of a list of Statements.
 type Program = [Statement]
 
+-- | A LANG statement is one of:
+--   - Print, which writes the value of an integer expression to the console
+--   - String print, which echoes a string to the console
+--   - Read, which reads an integer value from the console into a variable
+--   - Assign, which stores the value of an integer expression into a variable
+--   - While, a while loop with a control expression and a single-Statement body
+--   - If-Then, a conditional statement with a control expression and a single-Statement body
+--   - If-Then-Else, a conditional statement with a control expression, and two alternative
+--     single-Statement bodies
+--   - Compound statement that wrapes up multiple statements into a single statement for Whil
+--     or If-Then bodies
 data Statement = Print Expression
                | PrintString String
                | Read Ident
@@ -10,17 +22,23 @@ data Statement = Print Expression
                | IfThen Expression Statement
                | IfThenElse Expression Statement Statement
                | Compound [Statement]
-               | Empty
                deriving (Show)
 
+-- | A LANG expression is one of:
+--   - A variable reference
+--   - An integer constant
+--   - A binary operator expression
+--   - A unary operator expression
 data Expression = Var Ident
                 | Const Integer
                 | BinaryOp BinaryOperator Expression Expression
                 | UnaryOp UnaryOperator Expression
                 deriving (Show)
 
+-- | A LANG identifier simply wraps a string value.
 data Ident = Ident String deriving (Show, Eq)
 
+-- | These are the supported binary operators in LANG.
 data BinaryOperator = Add
                     | Sub
                     | Mul
@@ -37,6 +55,5 @@ data BinaryOperator = Add
                     | Or
                     deriving (Eq, Show, Enum)
 
+-- | This is the only supported unary operator in lang.
 data UnaryOperator = LogicalNegate deriving (Show)
-
-
--- assignment/implementation/Parser.hs
+++ assignment/implementation/Parser.hs
@@ -4,14 +4,23 @@ import AST
 import Parsing
 import Control.Applicative ((<$>), (<*>))
 
+-- | Utility function to parse a string as a program.
+--   Partially applies runP from the Parsing module with the
+--   the program parser.
+parseProgram :: String -> (Either ParseError a, String)
 parseProgram = runP program
 
+-- | Top-level parser for a LANG program.
+--   Requires parsing to complete with the parse buffer entirely
+--   consumed.
 program :: Parser Program
 program = do
     prog <- many statement
     eoi
     return prog
 
+-- | Parse a single LANG statement.
+--   Uses the option parser (<|>) to try each possible statement type.
 statement :: Parser Statement
 statement = printStatement
         <|> printStringStatement
@@ -22,33 +31,47 @@ statement = printStatement
         <|> ifThenStatement
         <|> compoundStatement
 
+-- | Print statement parser
+--   > print 5 + 5
 printStatement = do
     strToken "print"
     expr <- expression
     return $ Print expr
 
+-- | String print statement parser
+--   > sprint "hello"
 printStringStatement = do
     strToken "sprint"
     str <- stringLiteral
     return $ PrintString str
     
+-- | Read statement parser
+--   > read var
 readStatement = do
     strToken "read"
     ident <- identifier
     return $ Read ident
 
+-- | Assignment statement parser
+--   > var = 5 + 5
 assignStatement = do
     ident <- identifier
     strToken "="
     expr <- expression
     return $ Assign ident expr
 
+-- | While statement parser
+--   > while i < 10
+--   >     i = i + 1
 whileStatement = do
     strToken "while"
     expr <- expression
     stmt <- statement
     return $ While expr stmt
 
+-- | If-then statement parser
+--   > if x < 10 then
+--   >     print "Less than"
 ifThenStatement = do
     strToken "if"
     expr <- expression
@@ -56,6 +79,11 @@ ifThenStatement = do
     trueStmt <- statement
     return $ IfThen expr trueStmt
 
+-- | If-then-else statement parser
+--   > if x < 10 then
+--   >     print "Less than"
+--   > else
+--   >     print "Not less than"
 ifThenElseStatement = do
     strToken "if"
     expr <- expression
@@ -65,12 +93,24 @@ ifThenElseStatement = do
     falseStmt <- statement
     return $ IfThenElse expr trueStmt falseStmt
 
+-- | Compound statement parser
+--   > while i < 10 {
+--   >     i = i + 1
+--   >     print i
+--   > }
 compoundStatement = do
     strToken "{"
     stmts <- many statement
     strToken "}"
     return $ Compound stmts
 
+-- | Expression parser
+--   An expression consists of a term (a variable reference, a constant,
+--   a unary operator expression, or a parenthesized binary operator expression)
+--   followed, optionally, by the rest of a binary operator expression. If we
+--   fail to parse the tail, we have a simple term and the parse succeeds
+--   with that term as the result. Otherwise, we construct the AST value for
+--   the binary operator we found.
 expression = do
     t <- term
     tail <- optional termTail
@@ -79,6 +119,9 @@ expression = do
         Nothing      -> return t
         Just (op, u) -> return $ BinaryOp op t u
 
+-- | Parse the tail of a binary operator expression.
+--   This is just a binary operator symbol followed
+--   by another term.
 termTail = do
     opString <- binaryOperator
     let op = case opString of
@@ -113,6 +156,9 @@ termTail = do
                        <|> strToken "&&"
                        <|> strToken "||"
 
+-- | Parse a term, i.e., a variable reference or or integer
+--   literal, a unary operator application, or a bracketed
+--   binary operator expression.
 term = varExpr
    <|> constExpr
    <|> unaryOpExpr
@@ -122,10 +168,14 @@ term = varExpr
            strToken ")"
            return expr
              
+-- | Parser for variable reference terms
 varExpr = Var <$> identifier
 
+-- | Parser for integer constant terms 
 constExpr = Const <$> integerLiteral
     
+-- | Parser for unary operator expressions
+--   Only supports logical negation.
 unaryOpExpr = do
     opString <- unaryOperator
     let op = case opString of
@@ -134,6 +184,7 @@ unaryOpExpr = do
     return $ UnaryOp op t
     where unaryOperator = strToken "!"
 
+-- | Parser for integer literals with negation using '~'
 integerLiteral :: Parser Integer
 integerLiteral = do
     negation <- optional $ char '~'
@@ -143,15 +194,17 @@ integerLiteral = do
                                 Just _  -> -1
     return $ sign * (read digits)
 
+-- | Parser for string literals without support for escape sequences.
+-- Strings can span newlines.
 stringLiteral :: Parser String
 stringLiteral = do
-    -- Does not yet support escaped quotes
     char '"'
     contents <- many $ otherThan ['"']
     char '"'
     whitespace
     return contents
 
+-- | Parser for identifiers
 identifier :: Parser Ident
 identifier = do
     first <- alpha
@@ -159,9 +212,14 @@ identifier = do
     whitespace
     return . Ident $ first : rest
 
+-- | Parser for a single alphabetic character (ASCII)
 alpha :: Parser Char 
 alpha = oneOf $ ['a' .. 'z'] ++ ['A' .. 'Z']
 
+-- | Parser for a single numeric character (ASCII)
+numeric :: Parser Char
 numeric = oneOf ['0' .. '9']
 
+-- | Parser for a single alphanumeric character (ASCII) in terms of alpha and numeric parsers.
+alphanumeric :: Parser Char
 alphanumeric = alpha <|> numeric
